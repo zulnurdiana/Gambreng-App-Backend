721777105306d11628a489d40cbec31e8b3c6cdd
fajarbh14 <hajargak@gmail.com>
2023-02-01 22:44:08 +0700

    fix signout

--- src/routes/api/auth.ts
+++ src/routes/api/auth.ts
@@ -1,9 +1,10 @@
-import { signIn, signUp, verifyAccount, sendChangePasswordEmail, verifyNewPassword } from "@/controllers/auth.controller";
+import { signIn, signUp, verifyAccount, sendChangePasswordEmail, verifyNewPassword, signOut } from "@/controllers/auth.controller";
 import { Router } from "express";
 
 const router = Router();
 
 router.post("/signup", signUp);
+router.post("/signout", signOut);
 router.post('/account/verify', verifyAccount)
 router.post('/signin', signIn);
 router.post('/password/send', sendChangePasswordEmail);
